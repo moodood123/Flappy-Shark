fbaf0f4ce91fa195d2ca38cea1113bf003d195f0
Sam de Paiva <sammydepaiva@gmail.com>
2025-09-09 11:37:18 -0700

    Added and implemented floating ui script

--- Assets/Scripts/UI/FloatingElement.cs
+++ Assets/Scripts/UI/FloatingElement.cs
@@ -1,38 +1,53 @@
+using System.Collections;
 using PrimeTween;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(EventTrigger))]
 public class FloatingElement : MonoBehaviour
 {
-    [SerializeField] private TweenSettings _selectSettings;
-    [SerializeField] private TweenSettings _deselectSettings;
-
     [SerializeField] private TweenSettings<float> _moveXSettings;
     [SerializeField] private TweenSettings<float> _moveYSettings;
     [SerializeField] private TweenSettings<Quaternion> _rotateSettings;
-
+    
     private RectTransform _rt;
 
     private void Awake()
     {
         _rt = GetComponent<RectTransform>();
     }
-    
+
     private void OnEnable()
     {
-        Tween.UIAnchoredPositionX(_rt, _moveXSettings);
-        Tween.UIAnchoredPositionX(_rt, _moveYSettings);
-        Tween.LocalRotation(_rt, _rotateSettings);
-    }
-    
-    public void OnHover()
-    {
-        
+        StartAnimation();
     }
 
-    public void OnUnhover()
+    private void StartAnimation()
     {
-        
+        StartCoroutine(PositionXLoop());
+        StartCoroutine(PositionYLoop());
+        StartCoroutine(RotationLoop());
+    }
+
+    private IEnumerator PositionXLoop()
+    {
+        while (true)
+        {
+            yield return Tween.UIAnchoredPositionX(_rt, _moveXSettings).ToYieldInstruction();
+        }
+    }
+    
+    private IEnumerator PositionYLoop()
+    {
+        while (true)
+        {
+            yield return Tween.UIAnchoredPositionY(_rt, _moveYSettings).ToYieldInstruction();
+        }
+    }
+
+    private IEnumerator RotationLoop()
+    {
+        while (true)
+        {
+            yield return Tween.LocalRotation(_rt, _rotateSettings).ToYieldInstruction();
+        }
     }
 }
